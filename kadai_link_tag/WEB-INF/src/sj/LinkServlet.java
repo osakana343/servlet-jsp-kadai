88f12ac38fb0b0497923fd759a341110c6d5a3b7
osakana343 <t.ohashi@co-net.co.jp>
2026-07-14 09:30:34 +0900

    Add files via upload

---
+++ kadai_link_tag/WEB-INF/src/sj/LinkServlet.java
@@ -0,0 +1,23 @@
+package sj;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LinkServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+
+		// リンクをクリックした後であることを示す属性を設定して転送
+		request.setAttribute("text", "Servletからデータを受信しました：侍太郎さん、こんにちは！");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+		dispatcher.forward(request, response);
+	}
+}
